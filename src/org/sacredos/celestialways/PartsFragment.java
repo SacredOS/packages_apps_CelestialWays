1ad6eac46d5eb04f8e8467a3185ecdde4a177e44
therootlord <igor_cestari@hotmail.com>
2018-04-06 01:17:46 +0000

    CelestialWays: Layout changes based on NitrogenSettings app

--- src/org/sacredos/celestialways/PartsFragment.java
+++ src/org/sacredos/celestialways/PartsFragment.java
@@ -40,8 +40,6 @@ import android.preference.PreferenceCategory;
 import android.preference.PreferenceManager;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
-import android.support.v13.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -55,47 +53,20 @@ import com.android.settings.dashboard.SummaryLoader;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.sacredos.celestialways.tabs.System;
-import org.sacredos.celestialways.PagerSlidingTabStrip;
-import org.sacredos.celestialways.tabs.StatusBar;
-import org.sacredos.celestialways.tabs.Recents;
-import org.sacredos.celestialways.tabs.Lockscreen;
-import org.sacredos.celestialways.tabs.NotificationsPanel;
-import org.sacredos.celestialways.tabs.Navigation;
-
 public class PartsFragment extends SettingsPreferenceFragment {
 
     private static final int MENU_HELP  = 0;
 
-    ViewPager mViewPager;
-    String titleString[];
-    ViewGroup mContainer;
-    PagerSlidingTabStrip mTabs;
-
-    static Bundle mSavedState;
-
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        mContainer = container;
-
-        View view = inflater.inflate(R.layout.celestial_ways, container, false);
-        mViewPager = (ViewPager) view.findViewById(R.id.pager);
-        mTabs = (PagerSlidingTabStrip) view.findViewById(R.id.tabs);
-        StatusBarAdapter StatusBarAdapter = new StatusBarAdapter(getFragmentManager());
-        mViewPager.setAdapter(StatusBarAdapter);
-        mTabs.setViewPager(mViewPager);
-
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.celestialways);
         setHasOptionsMenu(true);
-        return view;
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle saveState) {
-        super.onSaveInstanceState(saveState);
     }
 
     @Override
@@ -106,7 +77,6 @@ public class PartsFragment extends SettingsPreferenceFragment {
     @Override
     public void onResume() {
         super.onResume();
-        mContainer.setPadding(30, 30, 30, 30);
     }
 
     @Override
@@ -168,48 +138,6 @@ public class PartsFragment extends SettingsPreferenceFragment {
         public void onCancel(DialogInterface dialog) {
 
         }
-    }
-
-    class StatusBarAdapter extends FragmentPagerAdapter {
-        String titles[] = getTitles();
-        private Fragment frags[] = new Fragment[titles.length];
-
-        public StatusBarAdapter(FragmentManager fm) {
-            super(fm);
-            frags[0] = new StatusBar();
-            frags[1] = new NotificationsPanel();
-            frags[2] = new Navigation();
-            frags[3] = new Recents();
-	    frags[4] = new Lockscreen();
-            frags[5] = new System();
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return titles[position];
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return frags[position];
-        }
-
-        @Override
-        public int getCount() {
-            return frags.length;
-        }
-    }
-
-    private String[] getTitles() {
-        String titleString[];
-        titleString = new String[]{
-                    getString(R.string.status_bar_category),
-                    getString(R.string.notifications_panel_category),
-                    getString(R.string.navigation_category),
-                    getString(R.string.recents_category),
-                    getString(R.string.lockscreen_category),
-                    getString(R.string.system_category)};
-        return titleString;
     }
 
     private static class SummaryProvider implements SummaryLoader.SummaryProvider {
